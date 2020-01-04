c2deaf1982a4dbc1cfa156ef9bc0ab7d3b2659e0
supertinypeaunt <1176032017@qq.com>
2020-01-05 06:43:24 +0800

    首页渲染导航菜单4个

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,14 +1,52 @@
 import React, { PureComponent } from 'react';
 // 导入ant组件
-import { Carousel } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
 // 导入请求
 import { getSweiperData } from '../../api/home'
+
+// 导入样式
+import './index.scss'
+
+// 导入导航图片数据
+import Nav1 from '../../asstes/images/nav-1.png'
+import Nav2 from '../../asstes/images/nav-2.png'
+import Nav3 from '../../asstes/images/nav-3.png'
+import Nav4 from '../../asstes/images/nav-4.png'
+
+// 导航数据
+const navs = [
+    {
+      id: 1,
+      img: Nav1,
+      title: '整租',
+      path: '/home/a'
+    },
+    {
+      id: 2,
+      img: Nav2,
+      title: '合租',
+      path: '/home/b'
+    },
+    {
+      id: 3,
+      img: Nav3,
+      title: '地图找房',
+      path: '/map'
+    },
+    {
+      id: 4,
+      img: Nav4,
+      title: '去出租',
+      path: '/rent/add'
+    }
+  ]
 
 class index extends PureComponent {
   state = {
       // 轮播图数据
       swiperData: [],
       imgHeight: 176,
+      autoplay:false
   }
 
    /**************** 模版渲染函数 *********************/
@@ -36,6 +74,26 @@ class index extends PureComponent {
     )
   }
 
+  // 渲染导航菜单4个
+  renderNavs = () => {
+    return navs.map(item => {
+      return (
+        <Flex.Item
+          className="nav"
+          key={item.id}
+          // 第三方组件->自带的事件->尝试写原生事件onClick
+          onClick={() => {
+            // 目前还没写路由配置(只有辨识)--------TODO
+            this.props.history.push(item.path)
+          }}
+        >
+          <img src={item.img} alt="" />
+          <p>{item.title}</p>
+        </Flex.Item>
+      )
+    })
+  }
+
   /******************** 请求函数 ********************/ 
   // 获取轮播图
   async getSwiper(){
@@ -49,11 +107,10 @@ class index extends PureComponent {
     // 更新数据状态
     this.setState(()=>{
       return {
-        swiperData: data.body
+        swiperData: data.body,
+        autoplay: true
       }
     })
-    console.log(data);
-    
   }
 
 
@@ -66,12 +123,12 @@ class index extends PureComponent {
     return (
       <>
         {/* 轮播图 */}
-        <Carousel
-        autoplay={this.state.swiperData.length}
-        infinite
-        >
+        <Carousel autoplay={this.state.autoplay} infinite >
           {this.renderSwiper()}
         </Carousel>
+
+        {/* 4个菜单 */}
+        <Flex>{this.renderNavs()}</Flex>
       </>
     )
   }
