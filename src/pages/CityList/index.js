ef557fbf1528c03063a50a256d2cac0b25328b26
supertinypeaunt <1176032017@qq.com>
2020-01-22 01:48:23 +0800

    城市选择-布局-列表组件-渲染

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -15,53 +15,6 @@ import './index.scss'
 
 // const Item = List.Item
 
-// 列表数据
-const list = [
-  '----Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn',
-  'Brian Vaughn'
-];
-
 class index extends PureComponent {
   state = {
     // 城市列表数据
@@ -74,57 +27,72 @@ class index extends PureComponent {
     showCityList: {}
   }
 
-  rowRenderer({
+  // 渲染每一行数据 
+  rowRenderer = ({
     key, // Unique key within array of rows
     index, // Index of row within collection
     isScrolling, // The List is currently being scrolled
     isVisible, // This row is visible within the List (eg it is not an overscanned row)
     style, // Style object to be applied to row (to position it)
-  }) {
+  }) => {
+    const { showCityList, orderIndex } = this.state
     return (
-      <div key={key} style={style}>
-        {list[index]}
+      <div key={key} style={style} className="city">
+        {/* 对当前索引地址城市渲染 */}
+        <div className="title">{ orderIndex[index] }</div>
+        {showCityList[orderIndex[index]].map( cityItem => 
+          <div className="name" key={cityItem.value} 
+          >{cityItem.label}</div>
+        )}
       </div>
-    );
+    )
   }
 
   // 渲染展示数据列表
-  renderList(){
-    // return (
-    //   // 根据顺序渲染列表
-    //   this.state.orderIndex.map((item, index) => {
-    //     return (
-    //       <List key={index} renderHeader={() => item} className="my-list">
+  // renderList(){
+  //   return ( 
+  //     // 根据顺序渲染列表
+  //     this.state.orderIndex.map((item, index) => {
+  //       return (
+  //         <List key={index} renderHeader={() => item} className="my-list">
 
-    //         {/* 渲染每一个字母所有城市 */}
-    //         {this.state.showCityList[item].map(cityItem => {
-    //           return (
-    //             <Item 
-    //               key={cityItem.value}
-    //               // 切换当前城市
-    //               onClick={async ()=> {
-    //                 // 查询当前城市是否有房源
-    //                 const { data }= await getHouseData(cityItem.value)
+  //           {/* 渲染每一个字母所有城市 */}
+  //           {this.state.showCityList[item].map(cityItem => {
+  //             return (
+  //               <Item 
+  //                 key={cityItem.value}
+  //                 // 切换当前城市
+  //                 onClick={async ()=> {
+  //                   // 查询当前城市是否有房源
+  //                   const { data }= await getHouseData(cityItem.value)
                     
-    //                 // 判断选择城市是否有房源
-    //                 if (data.body && data.body.length === 0) {
-    //                   return Toast.info('暂无房源', 1)
-    //                 }
+  //                   // 判断选择城市是否有房源
+  //                   if (data.body && data.body.length === 0) {
+  //                     return Toast.info('暂无房源', 1)
+  //                   }
 
-    //                 // 持久化本地存储
-    //                 setCurrentCityStorage(cityItem)
-    //                 this.props.history.goBack()
-    //               }
-    //               }
-    //               >
-    //                 {cityItem.label}
-    //             </Item>
-    //           )
-    //         })}
-    //       </List>
-    //     )
-    //   })
-    // )
+  //                   // 持久化本地存储
+  //                   setCurrentCityStorage(cityItem)
+  //                   this.props.history.goBack()
+  //                 }
+  //                 }
+  //                 >
+  //                   {cityItem.label}
+  //               </Item>
+  //             )
+  //           })}
+  //         </List>
+  //       )
+  //     })
+  //   )
+  // }
+
+
+  // 当个元素高度
+  renderItemHeight = ({ index }) =>{
+    const { showCityList, orderIndex } = this.state
+    const itemHeight = (showCityList[orderIndex[index]].length * 45)
+    return 45 + itemHeight
   }
 
   // 获取城市列表数据
@@ -211,8 +179,8 @@ class index extends PureComponent {
             <List
               width={width}
               height={height}
-              rowCount={list.length}
-              rowHeight={90}
+              rowCount={this.state.orderIndex.length}
+              rowHeight={this.renderItemHeight}
               rowRenderer={this.rowRenderer}
             />
           )}
